21cdef673e578bed6f6313e3cbea375953a47e9c
MasoomAnsari23 <masoom6ansari@gmail.com>
2024-04-27 11:04:08 +0530

    arrays 2D

---
+++ arrays/2-D-array-m1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A[3][4];   //declaration 3=row, 4=column
+    A[2][3]=15;   // initializing vale at row 2, column 3 =15
+   
+   int B[4][5]={{2,4,6,8,10},{3,6,9,12,18},{1,2,3,4,5},{9,18,27,36,45}};
+   //this is initailization and declaration
+}
