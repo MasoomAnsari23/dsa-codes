6ad586115b362d3e29883d1e05f86c02d79dd7ff
MasoomAnsari23 <masoom6ansari@gmail.com>
2026-01-14 15:29:46 +0530

    area of circle

---
+++ deloitte practice/areaOfCircle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float pi=3.14;
+    cout<<"enter radius of circle "<<endl;
+    int r;
+    cin>>r;
+    cout<<"area of circle is "<<pi*r*r<<endl;
+}
